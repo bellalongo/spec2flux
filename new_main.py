89bdd1a1aee2fd7bda4cb4d82228baa972a5183f
bellalongo <islo9452@colorado.edu>
2024-06-26 13:13:43 -0600

    adding gitignore

--- new_main.py
+++ new_main.py
@@ -1,10 +1,35 @@
+import matplotlib.pyplot as plt
 
+
+from spectrum_data import *
 
 
 def new_main():
     print('Loading new_main.py')
 
+    # Spectrum details (adjust me with each star)
+    spectrum_dir = 'spectra/ex_fits_file.fits'
+    rest_dir = 'DEM_goodlinelist.csv'
+    instrument = 'stis'
+    grating = 'e140m'
+    star_name = 'example'
+    min_wavelength = 1160
+
+    # User adjustable parameters
+    apply_smoothing = False # True if want to apply gaussian smoothing
+    fresh_start = True # True if first time running, or have already ran for a star and want to see final plot
+
+    # Load spectrum data
+    spectrum = SpectrumData(spectrum_dir, rest_dir, instrument, grating, star_name, min_wavelength, apply_smoothing)
+
+    # next function should take in spectrum as an object
+
+    # Sanity check
+    plt.plot(spectrum.wavelength_data, spectrum.flux_data)
+    plt.show()
+
 
 if __name__ == '__main__':
     new_main()
+
     print('All done!')
--- spectrum_data.py
+++ spectrum_data.py
@@ -2,10 +2,14 @@ import astropy.io.fits as fits
 from datetime import date
 import math
 import pandas as pd
+from scipy.ndimage import gaussian_filter
 
 
 class SpectrumData(object):
-    def __init__(self, spectrum_dir, rest_dir, instrument, grating, star_name, min_wavelength):
+    def __init__(self, spectrum_dir, rest_dir, 
+                 instrument, grating, star_name, 
+                 min_wavelength, smooth_data):
+        
         self.filename = spectrum_dir
         self.instrument = instrument.upper()
         self.grating = grating.upper()
@@ -14,11 +18,11 @@ class SpectrumData(object):
         self.todays_date = str(date.today())
 
         # Fetch data
-        data = fits.get_data(spectrum_dir)
+        data = fits.getdata(spectrum_dir)
         wavelength_data, flux_data, error_data = data['WAVELENGTH'], data['FLUX'], data['ERROR']
 
         # Mask wavelengths under min_wavelength
-        mask = wavelength_data[wavelength_data > min_wavelength]
+        mask = (wavelength_data > min_wavelength)
         self.wavelength_data, self.flux_data, self.error_data = wavelength_data[mask], flux_data[mask], error_data[mask]
 
         # Get rest wavelengths
@@ -31,6 +35,11 @@ class SpectrumData(object):
 
         # Get average peak width
         self.line_width, self.line_width_pixels, self.flux_range = self.peak_width_finder(grating, wavelength_data)
+
+        # Check if data needs to be smoothed
+        if smooth_data:
+            self.wavelength_data, self.flux_data, self.error_data = smooth_data(self.wavelength_data, self.flux_data, self.error_data, 1)
+        
 
 
     def peak_width_finder(self, grating, wavelength_data):
@@ -58,7 +67,19 @@ class SpectrumData(object):
         return peak_width, peak_width_pixels, flux_range
     
 
-    def smooth_data(wavelength_data, flux_data, error_data, sigma):
+    def smooth_data(self, wavelength_data, flux_data, error_data, sigma):
+        """
+            Smooths data using gaussian_filter
+            Parameters: 
+                        wavelength_data: masked wavelength data from the spectra  
+                        flux_data: masked flux data from the spectra   
+                        error_data: masked error data from the spectra  
+                        sigma: standard deviation of the gaussian (controls how much neighboring data points contribute to smoothing)
+            Returns:
+                        smoothed_wavelength: smoothed wavelength data
+                        smoothed_flux: smoothed flux data
+                        smoothed_error: smoothed error data
+        """
         smoothed_wavelength = gaussian_filter(wavelength_data, sigma)
         smoothed_flux = gaussian_filter(flux_data, sigma)
         smoothed_error = gaussian_filter(error_data, sigma)
